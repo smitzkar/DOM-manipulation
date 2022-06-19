2444e66dc4922204d87aee4e63a095447100d9c8
smitzkar <karl.e.schmitz@hotmail.com>
2022-06-19 13:02:44 +0200

    Init main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+// Simple code to add a new div (with class and content) as child to an existing one.
+
+// Get reference to existing div by id, store in container variable
+const container = document.querySelector('#container');
+
+// Create new div, store in content variable
+const content = document.createElement('div');
+// Add a class (named 'content'), then add textContent to it
+content.classList.add('content');
+content.textContent = 'Lorem ipsum, etc.';
+
+// Append new div to container  
+container.appendChild(content);
